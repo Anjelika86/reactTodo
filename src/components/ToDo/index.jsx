3993897af87f4369b1edc92a43ec572453e0433c
Anjelika <fominaangelika86@gmail.com>
2021-10-18 10:46:23 +0300

    Fix ToDo component

--- src/components/ToDo/index.jsx
+++ src/components/ToDo/index.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import TodoList from "./TodoList";
 import styles from "./Todo.module.scss";
 import { TODO_SCHEMA } from "../../utils/validationSchemas";
 
-const ToDo = ({ todos, setTodos }) => {
+const ToDo = () => {
+  const [todos, setTodos] = useState([]);
   const addTask = (text) => {
     const newTask = {
       id: Date.now(),
@@ -24,25 +25,34 @@ const ToDo = ({ todos, setTodos }) => {
 
   return (
     <div className={styles.todoContainer}>
-      <h1 className={styles.totoHeading}>ToDo List</h1>
-      <Formik
-        validationSchema={TODO_SCHEMA}
-        initialValues={{ taskText: "" }}
-        onSubmit={submitForm}
-      >
-        {(props) => {
-          return (
-            <Form>
-              <Field type="text" name="taskText" />
-              <ErrorMessage name="taskText">
-                {(message) => <div>{message}</div>}
-              </ErrorMessage>
-              <button type="submit">Add</button>
-            </Form>
-          );
-        }}
-      </Formik>
-      <TodoList todos={todos} setTodos={setTodos} />
+      <h1 className={styles.todoHeading}>ToDo List</h1>
+      <div className={styles.todoWrapper}>
+        <Formik
+          validationSchema={TODO_SCHEMA}
+          initialValues={{ taskText: "" }}
+          onSubmit={submitForm}
+        >
+          {(props) => {
+            return (
+              <Form className={styles.formContainer}>
+                <Field
+                  className={styles.fieldText}
+                  type="text"
+                  name="taskText"
+                  placeholder="New task"
+                />
+                <ErrorMessage name="taskText">
+                  {(message) => <div>{message}</div>}
+                </ErrorMessage>
+                <button className={styles.btnSubmit} type="submit">
+                  Add task
+                </button>
+              </Form>
+            );
+          }}
+        </Formik>
+        <TodoList todos={todos} setTodos={setTodos} />
+      </div>
     </div>
   );
 };
